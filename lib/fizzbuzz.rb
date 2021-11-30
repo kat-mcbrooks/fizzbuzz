32271e77815524da7b89079c4d09ea249dd97aba
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2021-11-30 18:33:36 +0000

    rspec tests and fizzbuzz code passes tests

---
+++ lib/fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(number)
+  if number % 3 == 0 && number % 5 == 0
+    return "fizzbuzz"
+  elsif number % 3 == 0
+    return "fizz"
+  elsif number % 5 == 0
+    return "buzz"
+  else
+    return number
+  end
+end
+
+fizzbuzz(4)
